afd7e73ebe5cdda0aad327b7bb4b089b5929b555
AmiKonst <tiufyakin@gmail.com>
2025-10-29 16:13:31 +0300

    fix pane bug

---
+++ assets/Genres-D88e14NM.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-DtYdv5eV.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-8YKMJylC.js';
+import './PageHeader-7_JFfJiw.js';
+import './ViewMode-DYAnuqzw.js';
+import './MultilingualField-BJT87XFj.js';
+import './FileUploader-CxpOhujb.js';
+
+const _sfc_main = {
+  __name: 'Genres',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "genres"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genres-D88e14NM.js.map

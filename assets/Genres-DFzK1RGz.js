af5f166abf54188f561dbc946d77e6d5ddbfb3ac
AmiKonst <tiufyakin@gmail.com>
2025-10-29 14:08:45 +0300

    audio worklet

---
+++ assets/Genres-DFzK1RGz.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-CC2cUZ7e.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-qltEKt2n.js';
+import './PageHeader-DshTodM0.js';
+import './ViewMode-CJGLxNxD.js';
+import './MultilingualField-BEwSvO0M.js';
+import './FileUploader-DbKXwurD.js';
+
+const _sfc_main = {
+  __name: 'Genres',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "genres"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genres-DFzK1RGz.js.map

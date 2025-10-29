ee1caacfc2a65c5f31946de350eebf1e5565602e
AmiKonst <tiufyakin@gmail.com>
2025-10-29 14:32:06 +0300

    is mobile

---
+++ assets/Categories-BIg1yn1_.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-Ch9OHVDL.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-XTe9m7FD.js';
+import './PageHeader-Dp58SVJr.js';
+import './ViewMode-DDliXRri.js';
+import './MultilingualField-C7kE9aY6.js';
+import './FileUploader-Dr3ZJs2E.js';
+
+const _sfc_main = {
+  __name: 'Categories',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "categories"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Categories-BIg1yn1_.js.map

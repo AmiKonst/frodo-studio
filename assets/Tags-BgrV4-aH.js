6344c1e2e4b1190ff634fabf81a97f73158965cc
AmiKonst <tiufyakin@gmail.com>
2025-10-29 14:40:05 +0300

    lock mobile

---
+++ assets/Tags-BgrV4-aH.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-YvTm51WG.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-Bhde94BR.js';
+import './PageHeader-BiJZFlUf.js';
+import './ViewMode-BqaQHvf5.js';
+import './MultilingualField-BTe3CQ50.js';
+import './FileUploader-DvdRAR6r.js';
+
+const _sfc_main = {
+  __name: 'Tags',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "tags"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Tags-BgrV4-aH.js.map

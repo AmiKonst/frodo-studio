d4a621c11f03e4bd85031e74cd401d56aa19a2fc
AmiKonst <tiufyakin@gmail.com>
2025-11-10 12:43:36 +0300

    build

---
+++ assets/Tags-DeeR-QBp.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-7iaY_FAG.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-CB0hEQSU.js';
+import './PageHeader-CKOcrPUo.js';
+import './ViewMode-py3Xzb81.js';
+import './MultilingualField-DQjSW80M.js';
+import './FileUploader-DT3vBmzF.js';
+
+const _sfc_main = {
+  __name: 'Tags',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "tags"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Tags-DeeR-QBp.js.map

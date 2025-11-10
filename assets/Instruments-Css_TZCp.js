4572769a732a2c83005daaafe01a03c5491c9c70
AmiKonst <tiufyakin@gmail.com>
2025-11-10 13:38:40 +0300

    build

---
+++ assets/Instruments-Css_TZCp.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-CGD0fnIa.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-C4OTGwdc.js';
+import './PageHeader-DjFM676m.js';
+import './ViewMode-Dw2IhLfk.js';
+import './MultilingualField-13oUNyOx.js';
+import './FileUploader-B-nhnF3q.js';
+
+const _sfc_main = {
+  __name: 'Instruments',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "instruments"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Instruments-Css_TZCp.js.map

50c131bcc476b56e3e3980162418e481b06fc6a8
AmiKonst <tiufyakin@gmail.com>
2025-11-10 13:51:02 +0300

    debug

---
+++ assets/Categories-COZFiKS2.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-BeDfLNVt.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-6yNOSgKE.js';
+import './PageHeader-5O8UU2_t.js';
+import './ViewMode-BC8JdJoB.js';
+import './MultilingualField-H_3Ua3gd.js';
+import './FileUploader-ND-sBMBt.js';
+
+const _sfc_main = {
+  __name: 'Categories',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "categories"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Categories-COZFiKS2.js.map

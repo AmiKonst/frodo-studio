182d88fead5c52c98d922837ca858c5fc5467a29
AmiKonst <tiufyakin@gmail.com>
2025-11-10 13:47:19 +0300

    build

---
+++ assets/Genres-BzmZs_E-.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-h6XLI9oE.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-CrWbz-pI.js';
+import './PageHeader-CcX0jLmR.js';
+import './ViewMode-DJcS8bLj.js';
+import './MultilingualField-wjtv1nzX.js';
+import './FileUploader-Cz685c93.js';
+
+const _sfc_main = {
+  __name: 'Genres',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "genres"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genres-BzmZs_E-.js.map

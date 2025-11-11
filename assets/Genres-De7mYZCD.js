094a0ed9380518eade1dc9e04cb7ebe427551568
AmiKonst <tiufyakin@gmail.com>
2025-11-11 12:14:00 +0300

    build

---
+++ assets/Genres-De7mYZCD.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-DCC3_px5.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-CuriYjet.js';
+import './PageHeader-k1bkUrc5.js';
+import './ViewMode-BTuonIKr.js';
+import './MultilingualField-Crotk4df.js';
+import './FileUploader-XsAo6vke.js';
+
+const _sfc_main = {
+  __name: 'Genres',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "genres"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genres-De7mYZCD.js.map

71c80ae4c5ec9f6839e9acd5e6a2e1ca73b36ad2
AmiKonst <tiufyakin@gmail.com>
2025-11-12 11:16:59 +0300

    build

---
+++ assets/Tags-BFgdOedO.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-CZztxjjE.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-D4RMqZs5.js';
+import './PageHeader-BEgsLcfl.js';
+import './ViewMode-BiQHk1eh.js';
+import './MultilingualField-D8spRJZH.js';
+import './FileUploader-mG_CKRpq.js';
+
+const _sfc_main = {
+  __name: 'Tags',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "tags"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Tags-BFgdOedO.js.map

d2b845c052f2cf852f8385d754939b6f3299a109
AmiKonst <tiufyakin@gmail.com>
2025-11-18 21:00:28 +0300

    build

---
+++ assets/Instruments-BWJ444tC.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-BQXYtDfR.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-BVCFx2RD.js';
+import './PageHeader-BINLpoLc.js';
+import './ViewMode-5pJSHa8t.js';
+import './MultilingualField-CtkP9WZ9.js';
+import './FileUploader-CLWj_tJQ.js';
+
+const _sfc_main = {
+  __name: 'Instruments',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "instruments"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Instruments-BWJ444tC.js.map

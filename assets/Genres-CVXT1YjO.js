11471549283966e6df604780fc9830de663a6bed
AmiKonst <tiufyakin@gmail.com>
2025-11-21 14:41:31 +0300

    build

---
+++ assets/Genres-CVXT1YjO.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-BYjBSrkg.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-Bv2uqDXL.js';
+import './PageHeader-D4RiGviz.js';
+import './ViewMode-giXCQTWr.js';
+import './MultilingualField-CibvJtXl.js';
+import './FileUploader-C_mNZ1DR.js';
+
+const _sfc_main = {
+  __name: 'Genres',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "genres"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genres-CVXT1YjO.js.map
---
+++ assets/Instruments-4lpCkziQ.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-BYjBSrkg.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-Bv2uqDXL.js';
+import './PageHeader-D4RiGviz.js';
+import './ViewMode-giXCQTWr.js';
+import './MultilingualField-CibvJtXl.js';
+import './FileUploader-C_mNZ1DR.js';
+
+const _sfc_main = {
+  __name: 'Instruments',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "instruments"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Instruments-4lpCkziQ.js.map

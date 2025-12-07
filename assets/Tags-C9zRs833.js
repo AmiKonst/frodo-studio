0629db8af4cf272bb0271bfeea64cded38eacd46
AmiKonst <tiufyakin@gmail.com>
2025-12-07 20:09:28 +0300

    build

---
+++ assets/Tags-C9zRs833.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-CM4bkpYj.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-Y7sL5H98.js';
+import './PageHeader-bf2kvL5x.js';
+import './ViewMode-BzqHOAml.js';
+import './MultilingualField-DOM4VCJv.js';
+import './FileUploader-DTc3T8BA.js';
+
+const _sfc_main = {
+  __name: 'Tags',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "tags"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Tags-C9zRs833.js.map

67881357568823dd7a925fe4af2cc9c3ebb5eeb3
AmiKonst <tiufyakin@gmail.com>
2026-01-14 13:30:46 +0300

    Build

---
+++ assets/Instruments-DfnMmZpE.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-DKCIttY9.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-C2EB-8pr.js';
+import './PageHeader-D4WLT7rq.js';
+import './ViewMode-BQIC1Huu.js';
+import './MultilingualField-Sg1yncu_.js';
+import './FileUploader-BmBr0di9.js';
+
+const _sfc_main = {
+  __name: 'Instruments',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "instruments"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Instruments-DfnMmZpE.js.map

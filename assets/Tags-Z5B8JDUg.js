0b53a8080205ec503383d4127b858a962baf47de
AmiKonst <tiufyakin@gmail.com>
2026-02-04 20:41:37 +0300

    Build

---
+++ assets/Tags-Z5B8JDUg.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-DWKSbCvA.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-ByUQ-40J.js';
+import './PageHeader-BERvVw6X.js';
+import './ViewMode-DH4jnAWg.js';
+import './MultilingualField-BHeMH_Pl.js';
+import './FileUploader-IiSI58AE.js';
+
+const _sfc_main = {
+  __name: 'Tags',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "tags"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Tags-Z5B8JDUg.js.map

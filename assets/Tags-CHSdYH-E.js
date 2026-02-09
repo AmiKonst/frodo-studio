2349cb311f35d0bb7bdab497e521bc7163120df0
AmiKonst <tiufyakin@gmail.com>
2026-02-09 13:13:36 +0300

    Build

---
+++ assets/Tags-CHSdYH-E.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-DHKX840D.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-CkGVcUEf.js';
+import './PageHeader-DUAJ-czi.js';
+import './ViewMode-BeBNNKIS.js';
+import './MultilingualField-DCnbpdPe.js';
+import './FileUploader-CCWeTUdP.js';
+
+const _sfc_main = {
+  __name: 'Tags',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "tags"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Tags-CHSdYH-E.js.map

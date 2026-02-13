8d65f68b5e7c29f674256ed2ac35221842e5ee3d
AmiKonst <tiufyakin@gmail.com>
2026-02-13 15:40:23 +0300

    Build

---
+++ assets/Instruments-T2UGEox7.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-B-sCOluP.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-B-miHGJX.js';
+import './PageHeader-BePnJjhT.js';
+import './ViewMode-DJ9schQy.js';
+import './MultilingualField-BxHPaCn3.js';
+import './FileUploader-CM937JyF.js';
+
+const _sfc_main = {
+  __name: 'Instruments',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "instruments"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Instruments-T2UGEox7.js.map

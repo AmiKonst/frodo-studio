7b4a4458921b89cae05dad1bd33c8def765bbf09
AmiKonst <tiufyakin@gmail.com>
2026-02-13 15:22:04 +0300

    Build

---
+++ assets/Tags-DcMXveFD.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-Y2gJBHcy.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-DDrckZKQ.js';
+import './PageHeader-DqVvvSmE.js';
+import './ViewMode-CgkfuSB5.js';
+import './MultilingualField-D557I8Nm.js';
+import './FileUploader-w4aW4Ku9.js';
+
+const _sfc_main = {
+  __name: 'Tags',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "tags"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Tags-DcMXveFD.js.map

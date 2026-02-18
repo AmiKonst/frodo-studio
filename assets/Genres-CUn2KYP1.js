1ccd0820b4a854ad00458826f029c030c66032e9
AmiKonst <tiufyakin@gmail.com>
2026-02-19 00:02:42 +0300

    Build

---
+++ assets/Genres-CUn2KYP1.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-SoOWu31N.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-Bsv1oAOe.js';
+import './PageHeader-DG0KpkSK.js';
+import './ViewMode-DDF3TBXi.js';
+import './MultilingualField-CTDze8ru.js';
+import './FileUploader-Bd0UEVw7.js';
+
+const _sfc_main = {
+  __name: 'Genres',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "genres"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genres-CUn2KYP1.js.map

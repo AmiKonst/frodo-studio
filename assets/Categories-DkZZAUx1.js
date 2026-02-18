7f6edf1375ecd4a6d99caac41ab22ae77c1d5c11
AmiKonst <tiufyakin@gmail.com>
2026-02-18 17:06:49 +0300

    Build

---
+++ assets/Categories-DkZZAUx1.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-DTv0Xtge.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-BhzJviFe.js';
+import './PageHeader-D3m-E7VO.js';
+import './ViewMode-C564TELh.js';
+import './MultilingualField-B1fEAd0-.js';
+import './FileUploader-CMOwQ7QS.js';
+
+const _sfc_main = {
+  __name: 'Categories',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "categories"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Categories-DkZZAUx1.js.map
---
+++ assets/Moods-Bf25pA6q.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-DTv0Xtge.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-BhzJviFe.js';
+import './PageHeader-D3m-E7VO.js';
+import './ViewMode-C564TELh.js';
+import './MultilingualField-B1fEAd0-.js';
+import './FileUploader-CMOwQ7QS.js';
+
+const _sfc_main = {
+  __name: 'Moods',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "moods"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Moods-Bf25pA6q.js.map

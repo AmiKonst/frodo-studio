a9bfb3ce3eec52c3ad3d06057abea869be28f236
AmiKonst <tiufyakin@gmail.com>
2026-02-20 15:45:11 +0300

    Build

---
+++ assets/Instruments-im5_cH70.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-C5-X9cXD.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-mtmarMBj.js';
+import './PageHeader-Dq3QOSXX.js';
+import './ViewMode-CN0UX4mw.js';
+import './MultilingualField-C7NQVmNg.js';
+import './FileUploader-CPX7bRZ8.js';
+
+const _sfc_main = {
+  __name: 'Instruments',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "instruments"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Instruments-im5_cH70.js.map
---
+++ assets/Tags-C7FqwZtj.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-C5-X9cXD.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-mtmarMBj.js';
+import './PageHeader-Dq3QOSXX.js';
+import './ViewMode-CN0UX4mw.js';
+import './MultilingualField-C7NQVmNg.js';
+import './FileUploader-CPX7bRZ8.js';
+
+const _sfc_main = {
+  __name: 'Tags',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "tags"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Tags-C7FqwZtj.js.map

217632de7d4a930633f55fa456943a1907596074
AmiKonst <tiufyakin@gmail.com>
2026-02-20 15:13:30 +0300

    Build

---
+++ assets/Genres-CuTwliEm.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-B_TAsy6Q.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-D7NaKvc4.js';
+import './PageHeader-wKNK_yTx.js';
+import './ViewMode-hIZzx2dC.js';
+import './MultilingualField-DN2-2fnz.js';
+import './FileUploader-D_Vd3aFv.js';
+
+const _sfc_main = {
+  __name: 'Genres',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "genres"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genres-CuTwliEm.js.map

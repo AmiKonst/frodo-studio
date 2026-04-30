bfc93aed697d0b6a76c90aba8373c69cae69a5e1
AmiKonst <tiufyakin@gmail.com>
2026-04-30 12:30:25 +0300

    Build

---
+++ assets/Instruments-CiGPqo4R.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-BCqMiolU.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-DNrh2hIS.js';
+import './PageHeader-CqYqASuq.js';
+import './ViewMode-BX66Tc7x.js';
+import './MultilingualField-BVe2g2j9.js';
+import './FileUploader-Bbb1VhLA.js';
+
+const _sfc_main = {
+  __name: 'Instruments',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "instruments"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Instruments-CiGPqo4R.js.map

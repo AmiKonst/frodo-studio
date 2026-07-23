124607ba5a2f37f4f987665d5548c52186f685a3
AmiKonst <tiufyakin@gmail.com>
2026-07-23 14:05:45 +0300

    Build

---
+++ assets/Moods-BZUHEFGA.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-l5-n7ikb.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-DjYaC0bW.js';
+import './PageHeader-DheSd9iu.js';
+import './ViewMode-Dn6jAG2l.js';
+import './MultilingualField-Bax2gXAs.js';
+import './FileUploader-DH5PWHyI.js';
+
+const _sfc_main = {
+  __name: 'Moods',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "moods"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Moods-BZUHEFGA.js.map

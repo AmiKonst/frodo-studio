5f8ab684c5db0a488c0a90a26824e90c02de8f7c
AmiKonst <tiufyakin@gmail.com>
2026-07-28 11:15:50 +0300

    Build

---
+++ assets/Categories-BA4QxZEv.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-BoKzU9ur.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-gcFB3s0p.js';
+import './PageHeader-CWqI2FEA.js';
+import './ViewMode-D75jkQTd.js';
+import './MultilingualField-DjmQNz0a.js';
+import './FileUploader-BE0okTEh.js';
+
+const _sfc_main = {
+  __name: 'Categories',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "categories"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Categories-BA4QxZEv.js.map

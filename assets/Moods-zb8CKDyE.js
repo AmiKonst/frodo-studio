f257399eb67237abee4b50242e4bffa6bf95275d
AmiKonst <tiufyakin@gmail.com>
2026-07-28 11:22:19 +0300

    Build

---
+++ assets/Moods-zb8CKDyE.js
@@ -0,0 +1,24 @@
+import { D as DICTIONARIES_SORT, a as Dictionaries } from './Dictionaries-BuIB5t4q.js';
+import { c as createBlock, o as openBlock, u as unref } from './index-DQdMtHMi.js';
+import './PageHeader-BBlMWFBY.js';
+import './ViewMode-BmHAzNXY.js';
+import './MultilingualField-C8JH45R3.js';
+import './FileUploader-BigCMdWm.js';
+
+const _sfc_main = {
+  __name: 'Moods',
+  setup(__props) {
+
+    
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(Dictionaries, {
+    sort: unref(DICTIONARIES_SORT),
+    dict: "moods"
+  }, null, 8, ["sort"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Moods-zb8CKDyE.js.map
